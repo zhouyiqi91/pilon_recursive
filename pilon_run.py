adfed7a58c01e42d4d580f70307ac60875a445e6
zhouyiqi <zhouyiqi@njlogin03.local>
2018-08-30 17:14:07 +0800

    second

--- pilon_run.py
+++ pilon_run.py
@@ -23,6 +23,7 @@ fastq_dir = infile["fastq_dir"]
 fasta = infile["fasta"]
 name = infile["name"]
 run_mode = infile["run_mode"].strip()
+iteration = int(infile["iteration"].strip())
 genome_size = os.path.getsize(fasta)
 
 bwa_cpu = bwa["cpu"]
@@ -38,75 +39,91 @@ pilon_mem = pilon["memory"]
 pilon_queue = pilon["queue"]
 pilon_opts = pilon["opts"].strip()
 
-out_dir = os.getcwd()
+base_dir = os.getcwd()
 
-if run_mode in ["all","script"]:
-	#bwa index
-	os.system("mkdir bwa")
-	os.chdir("bwa")
-	with open("index.sh",'w') as shell:
-		shell.write(shell_prefix+"""ln -s """+fasta+""" """+name+""".fasta
+if 0 < iteration < 11:
+	pass
+else:
+	exit("iteration should in [1..10]!")
+
+for run_time in range(1,iteration):
+	out_dir = base_dir +"/"+str(run_time)
+	fasta = run_once(out_dir,fastq_dir,fasta,name,run_mode,bwa_cpu,bwa_queue,bwa_opts,bwa_mem,pilon_cpu,pilon_mem,pilon_queue,pilon_opts)
+
+
+
+def run_once(out_dir,fastq_dir,fasta,name,run_mode,bwa_cpu,bwa_queue,bwa_opts,bwa_mem,pilon_cpu,pilon_mem,pilon_queue,pilon_opts):
+	if run_mode in ["all","script"]:
+		#bwa index
+		os.system("mkdir bwa")
+		os.chdir("bwa")
+		with open("index.sh",'w') as shell:
+			shell.write(shell_prefix+"""ln -s """+fasta+""" """+name+""".fasta
 bwa index """+name+""".fasta\n""")
-	new_fasta = os.getcwd()+"/"+name+".fasta"
+		new_fasta = os.getcwd()+"/"+name+".fasta"
 
-	#bwa mem
-	read1s = glob.glob(fastq_dir+"/*_1*.fq") + glob.glob(fastq_dir+"/*_1*.fq.gz") 
+		#bwa mem
+		read1s = glob.glob(fastq_dir+"/*_1*.fq") + glob.glob(fastq_dir+"/*_1*.fq.gz") 
 #+ glob.glob(fastq_dir+"/*_1*.fastq") + glob.glob(fastq_dir+"/*_1*.fastq.gz")
-	sorted_bams = []
-	with open("align.sh",'w') as shell:
-		shell.write(shell_prefix)
-		for read1 in read1s:
-			prefix = read1.split("/")[-1].split("_1")[0]
-			read2 = (glob.glob(fastq_dir+"/"+prefix+"_2*.fq")+glob.glob(fastq_dir+"/"+prefix+"_2*.fq.gz"))[0]
-			shell.write("""bwa mem -t """+str(bwa_cpu)+""" """+new_fasta+""" """+read1+""" """+read2+""" |samtools view -Sb - >"""+prefix+""".bam\n""")
-			shell.write("""samtools sort """+prefix+""".bam """+prefix+"""_sort\n""")
-			shell.write("""samtools index """+prefix+"""_sort.bam \n""")
-			sorted_bams.append("./bwa/"+prefix+"_sort.bam")
+		sorted_bams = []
+		with open("align.sh",'w') as shell:
+			shell.write(shell_prefix)
+			for read1 in read1s:
+				prefix = read1.split("/")[-1].split("_1")[0]
+				read2 = (glob.glob(fastq_dir+"/"+prefix+"_2*.fq")+glob.glob(fastq_dir+"/"+prefix+"_2*.fq.gz"))[0]
+				shell.write("""bwa mem -t """+str(bwa_cpu)+""" """+new_fasta+""" """+read1+""" """+read2+""" |samtools view -Sb - >"""+prefix+""".bam\n""")
+				shell.write("""samtools sort """+prefix+""".bam """+prefix+"""_sort\n""")
+				shell.write("""samtools index """+prefix+"""_sort.bam \n""")
+				sorted_bams.append("./bwa/"+prefix+"_sort.bam")
 
 	#pilon
-	frags_line = ""
-	for sorted_bam in sorted_bams:
-		frags_line += " --frags "+sorted_bam
-	pilon_line = "pilon -Xmx"+pilon_mem+" --diploid --changes --threads "+pilon_cpu+" --output "+name+" --genome "+new_fasta+frags_line
-	os.chdir(out_dir)
-	with open("pilon.sh",'w') as shell:
-		shell.write(shell_prefix)
-		shell.write(pilon_line+"\n")
+		frags_line = ""
+		for sorted_bam in sorted_bams:
+			frags_line += " --frags "+sorted_bam
+		pilon_line = "pilon -Xmx"+pilon_mem+" --diploid --changes --threads "+pilon_cpu+" --output "+name+" --genome "+new_fasta+frags_line
+		os.chdir(out_dir)
+		with open("pilon.sh",'w') as shell:
+			shell.write(shell_prefix)
+			shell.write(pilon_line+"\n")
 
-	print ("scripts generated.")
+		print ("scripts generated.")
 
 #run
-if run_mode in ["all","submit"]:
-	os.chdir("bwa")
-	#index
-	if not os.path.exists("index_done"):
-		cmd = "python "+bin_path+"sgearray.py -l vf="+bwa_mem+",p=1 -c 2 -q "+bwa_queue+" "+bwa_opts+" "+out_dir+"/bwa/index.sh"
-		print (cmd)
-		ret = subprocess.call(cmd,shell=True)
-		if ret == 0:
-			os.system("touch index_done")
-		else:
-			exit("index job failed")
-	#align
-	if not os.path.exists("align_done"):
-		cmd = "python "+bin_path+"sgearray.py -l vf="+bwa_mem+",p="+str(bwa_cpu)+" -c 3 -q "+bwa_queue+" "+bwa_opts+" "+out_dir+"/bwa/align.sh"
-		print (cmd)
-		ret = subprocess.call(cmd,shell=True)
-		if ret == 0:
-			os.system("touch align_done")
-		else:
-			exit("align job failed")
-	#pilon
-	os.chdir(out_dir)
-	if not os.path.exists("pilon_done"):
-		cmd = "python "+bin_path+"sgearray.py -l vf="+pilon_mem+",p="+str(pilon_cpu)+" -c 1 -q "+pilon_queue+" "+pilon_opts+" "+out_dir+"/pilon.sh"
-		print (cmd)
-		ret = subprocess.call(cmd,shell=True)
-		if ret == 0:
-			os.system("touch pilon_done")
-			print ("all pilon job done.")
-		else:
-			exit("pilon job failed")
+	if run_mode in ["all","submit"]:
+		os.chdir("bwa")
+		#index
+		if not os.path.exists("index_done"):
+			cmd = "python "+bin_path+"sgearray.py -l vf="+bwa_mem+",p=1 -c 2 -q "+bwa_queue+" "+bwa_opts+" "+out_dir+"/bwa/index.sh"
+			print (cmd)
+			ret = subprocess.call(cmd,shell=True)
+			if ret == 0:
+				os.system("touch index_done")
+			else:
+				exit("index job failed")
+		#align
+		if not os.path.exists("align_done"):
+			cmd = "python "+bin_path+"sgearray.py -l vf="+bwa_mem+",p="+str(bwa_cpu)+" -c 3 -q "+bwa_queue+" "+bwa_opts+" "+out_dir+"/bwa/align.sh"
+			print (cmd)
+			ret = subprocess.call(cmd,shell=True)
+			if ret == 0:
+				os.system("touch align_done")
+			else:
+				exit("align job failed")
+		#pilon
+		os.chdir(out_dir)
+		if not os.path.exists("pilon_done"):
+			cmd = "python "+bin_path+"sgearray.py -l vf="+pilon_mem+",p="+str(pilon_cpu)+" -c 1 -q "+pilon_queue+" "+pilon_opts+" "+out_dir+"/pilon.sh"
+			print (cmd)
+			ret = subprocess.call(cmd,shell=True)
+			if ret == 0:
+				os.system("touch pilon_done")
+				print ("all pilon job done.")
+			else:
+				exit("pilon job failed")
+
+		fasta = out_dir + "/" + name +".fasta" 
+
+		return fasta
 			
 
 
